09048411814415e786bd84ed142e35d1370a9fcd
Rossano-DAngelo <rossano.dangelo@ibm.com>
2019-08-23 12:59:56 +0100

    Setup formatting rules

--- src/components/HeadLine/HeadLine.test.js
+++ src/components/HeadLine/HeadLine.test.js
@@ -4,58 +4,58 @@ import HeadLine from './HeadLine';
 import { findByTestAttribute, checkProps } from '../../../Utils/index';
 
 const setUp = (props = {}) => {
-    const component = shallow(<HeadLine {...props} />);
-    return component;
+	const component = shallow(<HeadLine {...props} />);
+	return component;
 }
 
 describe('HeadLine component', () => {
-    describe('Checking propTypes', () => {
-        it('should not throw a warning', () => {
-            const expectedProps = {
-                header: 'Test Header',
-                description: 'Test description',
-                tempArray: [{
-                    firstName: 'Test Name',
-                    lastName: 'Test Last Name',
-                    email: 'Test email',
-                    age: 23,
-                    onlineStatus: false
-                }]
-            };
-            const propsErrors = checkProps(HeadLine, expectedProps);
-            expect(propsErrors).toBeUndefined();
-        });
-    });
-    describe('has props', () => {
-        let wrapper;
-        beforeEach(() => {
-            const props = {
-                header: 'Test Header',
-                description: 'Some Description'
-            };
-            wrapper = setUp(props);
-        });
-        it('should render without errors', () => {
-            const component = findByTestAttribute(wrapper, 'headLineComponent');
-            expect(component.length).toBe(1);
-        });
-        it('should render an header title', () => {
-            const h1 = findByTestAttribute(wrapper, 'headLineHeader');
-            expect(h1.length).toBe(1);
-        });
-        it('should render a description', () => {
-            const description = findByTestAttribute(wrapper, 'headLineDescription');
-            expect(description.length).toBe(1);
-        });
-    });
-    describe('has no props', () => {
-        let wrapper;
-        beforeEach(() => {
-            wrapper = setUp();
-        });
-        it('should not render', () => {
-            const component = findByTestAttribute(wrapper, 'headLineComponent');
-            expect(component.length).toBe(0);
-        });
-    });
+	describe('Checking propTypes', () => {
+		it('should not throw a warning', () => {
+				const expectedProps = {
+					header: 'Test Header',
+					description: 'Test description',
+					tempArray: [{
+						firstName: 'Test Name',
+						lastName: 'Test Last Name',
+						email: 'Test email',
+						age: 23,
+						onlineStatus: false
+					}]
+				};
+				const propsErrors = checkProps(HeadLine, expectedProps);
+				expect(propsErrors).toBeUndefined();
+		});
+	});
+	describe('has props', () => {
+			let wrapper;
+			beforeEach(() => {
+				const props = {
+					header: 'Test Header',
+					description: 'Some Description'
+				};
+				wrapper = setUp(props);
+			});
+			it('should render without errors', () => {
+					const component = findByTestAttribute(wrapper, 'headLineComponent');
+					expect(component.length).toBe(1);
+			});
+			it('should render an header title', () => {
+					const h1 = findByTestAttribute(wrapper, 'headLineHeader');
+					expect(h1.length).toBe(1);
+			});
+			it('should render a description', () => {
+					const description = findByTestAttribute(wrapper, 'headLineDescription');
+					expect(description.length).toBe(1);
+			});
+	});
+	describe('has no props', () => {
+			let wrapper;
+			beforeEach(() => {
+					wrapper = setUp();
+			});
+			it('should not render', () => {
+					const component = findByTestAttribute(wrapper, 'headLineComponent');
+					expect(component.length).toBe(0);
+			});
+	});
 });
